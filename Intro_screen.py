136eb4a65f9a118424f9dfec080f512a409218dc
Unknown <nol_computernerd@hotmail.com>
2020-06-12 12:57:03 +0200

    Made button function to clean up some hardcoding

--- Intro_screen.py
+++ Intro_screen.py
@@ -13,8 +13,6 @@ def game_intro():
     introbkgd = pg.image.load("images/mountains.png").convert()
     clock = pg.time.Clock()
     largetext = pg.font.Font('fonts/freesansbold.ttf', 115)
-    smalltext = pg.font.Font('fonts/freesansbold.ttf', 50)
-
 
     intro = True
     while intro:
@@ -35,43 +33,30 @@ def game_intro():
         text_objects("Data Gans", largetext, (screen_size[0]/2), (screen_size[1]/6))
 
         #Buttons
-        start_rect = pg.Rect((screen_size[0] / 2), (screen_size[1] / 2), 200, 75)
-        start_rect.center = (screen_size[0] / 2), (screen_size[1] / 2.5)
-        pg.draw.rect(main.SCREEN, white, start_rect)
-
-        rules_rect = pg.Rect((screen_size[0] / 2), (screen_size[1] / 2), 200, 75)
-        rules_rect.center = (screen_size[0] / 2), (screen_size[1] / 2)
-        pg.draw.rect(main.SCREEN, white, rules_rect)
-
-        hiscores_rect = pg.Rect((screen_size[0] / 2), (screen_size[1] / 2), 200, 75)
-        hiscores_rect.center = (screen_size[0] / 2), (screen_size[1] / 1.65)
-        pg.draw.rect(main.SCREEN, white, hiscores_rect)
-
-        #mouse collision with buttons
-        mouse = pg.mouse.get_pos()
-
-        if (screen_size[0] / 2) + 100 > mouse[0] > (screen_size[0] / 2) - 100 and (screen_size[1] / 2.5) + 32.75 > mouse[1] > (screen_size[1] / 2.5) - 32.75:
-            pg.draw.rect(main.SCREEN, brown, start_rect)
-
-        if (screen_size[0] / 2) + 100 > mouse[0] > (screen_size[0] / 2) - 100 and (screen_size[1] / 2) + 32.75 > mouse[1] > (screen_size[1] / 2) - 32.75:
-            pg.draw.rect(main.SCREEN, brown, rules_rect)
-
-        if (screen_size[0] / 2) + 100 > mouse[0] > (screen_size[0] / 2) - 100 and (screen_size[1] / 1.65) + 32.75 > mouse[1] > (screen_size[1] / 1.65) - 32.75:
-            pg.draw.rect(main.SCREEN, brown, hiscores_rect)
-
-        #Buttontext
-        text_objects("Start", smalltext, (screen_size[0] / 2), (screen_size[1] / 2.5))
-        text_objects("Regels", smalltext, (screen_size[0] / 2), (screen_size[1] / 2))
-        text_objects("Hiscore", smalltext, (screen_size[0] / 2), (screen_size[1] / 1.65))
+        button("Start", (screen_size[0] / 2), (screen_size[1] / 2.5), 200, 75, white, brown)
+        button("Regels", (screen_size[0] / 2), (screen_size[1] / 2), 200, 75, white, brown)
+        button("Hiscore", (screen_size[0] / 2), (screen_size[1] / 1.65), 200, 75, white, brown)
 
         pg.display.update()
-        clock.tick(120)
+        clock.tick(60)
     
-
 
 def text_objects(text, font, center_x, center_y):
     textsurface = font.render(text, True, black)
     textrect = textsurface.get_rect()
     textrect.center = (center_x, center_y)
     main.SCREEN.blit(textsurface, textrect)
-    #return textsurface, textsurface.get_rect()
+
+
+def button(msg,x,y,width,height,inactivecolor,activecolor):
+    smalltext = pg.font.Font('fonts/freesansbold.ttf', 50)
+    mouse = pg.mouse.get_pos()
+    rect = pg.Rect(x, y, width, height)
+    rect.center = (x, y)
+    if x + width > mouse[0] > x - width and y + height > mouse[1] > y - height:
+        pg.draw.rect(main.SCREEN, activecolor, rect)
+    else:
+        pg.draw.rect(main.SCREEN, inactivecolor, rect)
+
+    text_objects(msg, smalltext, x, y)
+
