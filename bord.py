03096af8b368bbee48cd228d1fa21f27beb558ee
Gina <ginadejong_@outlook.com>
2020-06-13 16:42:10 +0200

    Basics
    Before I deleted part of the code (bigWeh)

---
+++ bord.py
@@ -0,0 +1,40 @@
+import pygame as pg
+
+class Board:
+
+    def __init__(self, height, width, margin, row, column):
+        self.height = height
+        self.width = width
+        self.margin = margin
+        self.row = row
+        self.column = column
+        self.steps = []
+        self.grid = []
+        self.xy = []
+
+    def set_steps(self, amount):
+        for step in range(0, amount+1 ,1):
+            self.steps.append(step)
+
+    def get_steps(self):
+        return self.steps
+
+    def set_grid(self, positions, screen):
+        for rw in range(self.row):
+            self.grid.append([])
+            for clmn in range(self.column):
+                self.grid[rw].append(0)
+                location = [rw, clmn]
+                if location in positions:
+                    x, y = (self.margin + self.width) * clmn + self.margin, (self.margin + self.height) * rw + self.margin
+                    self.xy.append([x, y])
+                    pg.draw.rect(screen, (0,255,0),[x, y, self.width, self.height])
+              
+    def get_grid(self):
+        return self.grid
+
+    def set_xy(self): 
+        pass
+
+    def get_xy(self, step):
+        return self.xy[step]
